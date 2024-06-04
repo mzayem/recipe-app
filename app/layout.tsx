ca87bd4647e1f72011009c784f8d708caf6b2596
mzayem <mzayemazam@gmail.com>
2024-06-05 01:51:21 +0500

    top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import Navbar from "@/components/navbar";
+import NextTopLoader from "nextjs-toploader";
 import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <NextTopLoader />
           <Navbar />
           {children}
           <Footer />
