7f6ce088b4d284b460b0b08071ee3a001dd859ac
mzayem <mzayemazam@gmail.com>
2024-06-05 18:10:06 +0500

    image updated

--- components/banner.tsx
+++ components/banner.tsx
@@ -12,7 +12,7 @@ export default function Banner() {
       style={{
         backgroundImage: `url(${
           theme === "dark"
-            ? "https://img.freepik.com/free-photo/tasty-pizza-near-ingredients_23-2147772080.jpg?t=st=1717533553~exp=1717537153~hmac=4b9bb36b72be7d15ba3626122cb782d7deeb36857a64aea7ea27babe58617356&w=1380"
+            ? "https://img.freepik.com/free-photo/tasty-pizza-near-ingredients_23-2147772080.jpg?t=st=1717592749~exp=1717596349~hmac=92801333605523046a196af2eb808f886e9f4ef05be136d09d099772a3665a87&w=1380"
             : "https://img.freepik.com/free-photo/copy-space-italian-food-ingredients_23-2148551732.jpg?w=1380&t=st=1717533665~exp=1717534265~hmac=1d3ed73a2c4a47a2831a1d6560a545a48069987955c0ec1168432ab5870416e5"
         })`,
       }}
