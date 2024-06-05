96beb36d316fd42b61409645f651806591049ca0
mzayem <mzayemazam@gmail.com>
2024-06-05 17:59:50 +0500

    theme fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <NextTopLoader color="#000000" />
           <Navbar />
           {children}
